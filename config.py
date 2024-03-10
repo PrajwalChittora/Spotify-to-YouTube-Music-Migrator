4c2b5039fc91f997a2b736fac87f55c9bbbc1f62
PrajwalChittora <109464358+PrajwalChittora@users.noreply.github.com>
2024-03-11 02:05:05 +0530

    credentials.py

--- config.py
+++
@@ -1,4 +0,0 @@
-SPOTIFY_CLIENT_ID = "your_spotify_client_id"
-SPOTIFY_CLIENT_SECRET = "your_spotify_client_secret"
-YOUTUBE_CLIENT_ID = "your_youtube_client_id"
-YOUTUBE_CLIENT_SECRET = "your_youtube_client_secret"
---
+++ credentials.py
@@ -0,0 +1,4 @@
+yt_api = "your_youtube_client_id"
+my_client_id = "your_spotify_client_id"
+my_client_secret = "your_spotify_client_secret"
+YOUTUBE_CLIENT_SECRET = "your_youtube_client_secret"
