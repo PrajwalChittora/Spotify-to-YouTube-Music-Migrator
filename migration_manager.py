0d5db19d83b4a008f033ca69fe34b54daa02793c
PrajwalChittora <109464358+PrajwalChittora@users.noreply.github.com>
2024-03-11 01:55:08 +0530

    migration_manager.py

--- migration_manager.py
+++ migration_manager.py
@@ -1,22 +1,30 @@
-import spotify_api
-import youtube_api
 
-def migrate_playlist(spotify_auth, youtube_auth, playlist):
-    yt = youtube_api.get_youtube_auth()
-    yt_playlist = yt.playlists().insert(...).execute() 
+from credentials import my_client_secret, my_client_id,yt_api
+from spotify_extract import connect,fetch_playlist_by_id,extract_data,query_builder
+from ytOauth import makePlaylist, addItemToPlaylist, makeService
+from ytapi import getVideoIds
 
-    tracks = spotify_api.get_playlist_tracks(spotify_auth, playlist['id'])
-    for track in tracks:
-        search_result = youtube_api.search_track(yt, track['track']['name'], track['track']['artists'][0]['name'])
-        # Do something with the search result (find the best match)  
-        youtube_api.add_track_to_playlist(yt, yt_playlist['id'], search_result['id']['videoId'])
 
-def main():
-    sp = spotify_api.get_spotify_auth()
-    yt = youtube_api.get_youtube_auth()
-    playlists = spotify_api.get_user_playlists(sp)
-    for playlist in playlists:
-        migrate_playlist(sp, yt, playlist)
+playlist_id = "playlist_id"
 
-if __name__ == '__main__':
-    main()
+
+def main(pl_id):
+    api = connect(my_client_id,my_client_secret)
+    items_name = fetch_playlist_by_id(api,pl_id)
+    name = items_name[1]
+    queries = query_builder(extract_data(api,items_name[0]))
+    videoIds = getVideoIds(queries,yt_api)
+   
+    service = makeService()
+    new_play_id = makePlaylist(service,name)
+    addItemToPlaylist(service,new_play_id,videoIds)
+    print("finished migrating")
+    # return queries
+    return new_play_id
+
+
+
+print(main(playlist_id))
+
+
+
